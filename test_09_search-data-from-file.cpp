a9156ec27f042af04f04a0dad7185c7817cc1467
SumedhaEndar <1211200546@student.mmu.edu.my>
2022-04-02 02:49:32 +0800

    Update test_09_search-data-from-file.cpp

--- test_09_search-data-from-file.cpp
+++ test_09_search-data-from-file.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 class Employee
 {
-	private:
+	protected:
 		int jobType, id, exp, age;
 		string name, dob, contact_num, edu_level, date_in;
+		float initial_salary, base_salary, nett_salary, d;
 		
 	public:		
 		void setId(int &set_id)
@@ -64,9 +65,40 @@ class Employee
 			cout << " " << endl;
 		}
 		
-		int getId()
+};
+
+
+class Executive : public Employee
+{
+	private:
+		int bonus;
+		
+	public:
+		Executive()
 		{
-			return id;	
+			initial_salary = 2000; // bachelor initial salary
+ 			d = 300;			   // difference
+			base_salary = 0;
+			nett_salary = 0;
+		}
+		
+		void A_P_calculation()
+		{
+			if(edu_level.find("Diploma") == 0)
+			{
+				cout << edu_level.find("Diploma") << endl;
+				initial_salary = 1700;
+				d = 200;
+			}
+			
+			base_salary = initial_salary + ((exp)*(d));
+		};
+		
+		void display_base_salary()
+		{
+			A_P_calculation();
+			cout << fixed;
+			cout << " Base Salary\t: RM " << setprecision(2) << base_salary << endl;
 		}
 };
 
@@ -95,20 +127,45 @@ int num_of_lines_Blue()
 	return count;
 }	
 
+int num_of_lines_Exec()
+{
+	int count = 0;
+	string line;
+	ifstream myfile ("Exe_Data.txt");
+
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			count = count + 1;
+		}
+		myfile.close();		
+	}
+	
+	else 
+	{
+		cout << "Unable to open file"; 
+		count = 0;
+	}
+	
+	return count;
+}	
+
+
 
 int main()
 {
-	Employee *Emp;
+	Executive *Emp;
 	
-	ifstream myfile("Blue_Data.txt");
+	ifstream myfile("Exe_Data.txt");
 	
 	// TroubleSome !!!!!!!!!
 	int jobType, id, exp, age, compare_id;
 	string name, dob, contact_num, edu_level, date_in;
 	// End of TroubleSome
 	
-	int num_of_lines = num_of_lines_Blue();
-	Emp = new Employee[num_of_lines];
+	int num_of_lines = num_of_lines_Exec();
+	Emp = new Executive[num_of_lines];
  
  	cout << " Enter ID : ";
  	cin >> compare_id;
@@ -119,7 +176,7 @@ int main()
 		{
 			myfile >> jobType >> id >> name >> dob >> age >> 
 		    contact_num >> edu_level >> exp >> date_in;
-		    
+		    		    
 		    if(compare_id == id)
 		    {
 		    	Emp[i].setId(id);
@@ -131,17 +188,20 @@ int main()
 		    	Emp[i].setExp(exp);
 		    	Emp[i].setDateIn(date_in);
 		    	Emp[i].display();
+		    	Emp[i].display_base_salary();
+		    	
+		    	compare_id = 0;
 			}
-		}
-		
-		myfile.close();
+		}	
 	}
-	
+		
 	else
 	{
 		cout << " Unable to Open File " << endl;
 	}
 	
+	myfile.close();
+	delete [] Emp;
 	
 	return 0;
 }
