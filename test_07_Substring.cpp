5407d79bf04ae9e6d901d7f9eeb7a5b79d74516a
SumedhaEndar <1211200546@student.mmu.edu.my>
2022-04-01 14:02:43 +0800

    Create test_07_Substring.cpp

---
+++ test_07_Substring.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+//	string edu;
+	
+//	edu = "Diploma in Electronic Engineering";
+//	edu = "Bachelor of Electronic Engineering";
+	
+//	cout << edu.substr(0,7) << endl;
+//	cout << edu.find("Bachelor");
+//	cout << edu.find("Diploma");
+	
+	
+	string dob;
+	
+//	dob = "1-2-2000";   // substr(4) when length is 8
+//	dob = "11-2-2000";  // substr(5) when length is 9
+//	dob = "1-12-2000";  // substr(5) when length is 9		
+	dob = "11-12-2000"; // substr(6) when length is 10
+	
+	cout << dob.length() << endl;
+	cout << dob.substr(6) << endl;
+	
+// use stoi to convert string to int
+	cout << 2022 - stoi(dob.substr(6)) << endl;
+	
+	return 0;
+}
