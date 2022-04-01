fce640bc7812e49f7495eba094a94747aafe6866
SumedhaEndar <1211200546@student.mmu.edu.my>
2022-04-01 14:02:49 +0800

    Create test_06_Write-data-space-separated-to-a-file.cpp

---
+++ test_06_Write-data-space-separated-to-a-file.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+
+using namespace std;
+
+class Employee
+{
+	private:
+		int jobType, id, exp, age;
+		string name, dob, contact_num, edu_level, date_in;
+		
+	public:
+		void calAge()
+		{
+			
+		}
+		
+};
+
+int main()
+{
+	
+	
+	return 0;
+}
