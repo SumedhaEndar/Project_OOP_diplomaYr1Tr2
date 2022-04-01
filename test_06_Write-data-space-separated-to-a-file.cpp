5c536da712b1a3d1d2a56b3a00199cd477c1386a
SumedhaEndar <1211200546@student.mmu.edu.my>
2022-04-01 14:29:08 +0800

    Update test_06_Write-data-space-separated-to-a-file.cpp

--- test_06_Write-data-space-separated-to-a-file.cpp
+++ test_06_Write-data-space-separated-to-a-file.cpp
@@ -11,16 +11,65 @@ class Employee
 		string name, dob, contact_num, edu_level, date_in;
 		
 	public:
+		void setDetails()
+		{		
+			cout << " Enter Job Type\t: ";
+			cin >> jobType;
+			cout << " Enter ID\t: ";
+			cin >> id;
+			
+			cin.ignore();
+			
+			cout << " Enter Name\t: ";
+			getline(cin,name);
+			cout << " Enter D.O.B\t: ";
+			getline(cin,dob);
+			cout << " Enter Contact Number\t: ";
+			getline(cin,contact_num);
+			cout << " Enter Education Level\t: ";
+			getline(cin,edu_level);
+			cout << " Enter Years of Experience\t: ";
+			cin >> exp;
+			
+			cin.ignore();
+			
+			cout << " Enter Date-In\t: ";
+			getline(cin,date_in);	
+		}
+		
 		void calAge()
 		{
-			
+			switch(dob.length())
+			{
+				case 8:
+					age = 2022 - stoi(dob.substr(4));
+					cout << " Age \t: " << age <<endl;
+					break;
+				
+				case 9:
+					age = 2022 - stoi(dob.substr(5));
+					cout << " Age \t: " << age <<endl;
+					break;
+					
+				case 10:
+					age = 2022 - stoi(dob.substr(6));
+					cout << " Age \t: " << age <<endl;
+					break; 
+			}
 		}
 		
 };
 
 int main()
 {
+	Employee *Emp;
 	
+	Emp = new Employee;
+	
+	Emp->setDetails();
+	Emp->calAge();
+	
+	delete Emp;
 	
 	return 0;
 }
